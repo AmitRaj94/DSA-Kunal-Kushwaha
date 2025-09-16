fb0229a155983609cedf14bf1680105725910a07
amit251358 <amit251358@gmail.com>
2025-09-16 05:51:58 +0530

    16092025

---
+++ data structures and algorithms/src/calculators.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class calculators {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        for(int i = 0; i<=num1;i++){
+            System.out.println(i);
+        }
+
+    }
+}
