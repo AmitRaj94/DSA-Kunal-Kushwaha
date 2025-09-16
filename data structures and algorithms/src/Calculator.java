e88028b5eb6f3f4f6f93617df3325b41705a03be
amit251358 <amit251358@gmail.com>
2025-09-16 07:36:09 +0530

    16092025

--- data structures and algorithms/src/Calculator.java
+++ data structures and algorithms/src/Calculator.java
@@ -5,47 +5,49 @@ public class Calculator {
         Scanner input = new Scanner(System.in);
         char op;
 
-        do{
-            System.out.print("Enter the operation: ");
+        do {
+            System.out.print("Enter the operation (+, -, *, /, %, or x to exit): ");
             op = input.next().trim().charAt(0);
-            int result=0;
-            if(op=='+' || op=='-' || op=='*' || op=='/' || op=='%'){
+            int result = 0;
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
                 System.out.print("Enter the first number: ");
                 int num1 = input.nextInt();
                 System.out.print("Enter the second number: ");
                 int num2 = input.nextInt();
+
                 switch (op) {
                     case '+':
                         result = num1 + num2;
                         break;
                     case '-':
-                        result = num1-num2;
+                        result = num1 - num2;
                         break;
                     case '*':
-                      result=num1*num2;
+                        result = num1 * num2;
                         break;
                     case '/':
-                        if(num2==0){
-                            System.out.println("invalid division");
+                        if (num2 == 0) {
+                            System.out.println("Error: Division by zero is not allowed.");
                             continue;
                         }
-                        result=num1/num2;
+                        result = num1 / num2;
                         break;
-                    case '%' :
-                        result =num1%num2;
+                    case '%':
+                        result = num1 % num2;
                         break;
                 }
-            System.out.println("Result: "+result);
-            }
-            else if (op=='X' || op=='x') {
-                System.out.println("Exiting from calculator......");
+
+                System.out.println("Result: " + result);
+            } else if (op == 'X' || op == 'x') {
+                System.out.println("Exiting from calculator...");
                 break;
-            }
-            else{
-                System.out.println("invalid input. Please try again!!...");
+            } else {
+                System.out.println("Invalid input. Please try again!");
             }
 
-        }while (true);
+        } while (true);
+
         input.close();
     }
 }
